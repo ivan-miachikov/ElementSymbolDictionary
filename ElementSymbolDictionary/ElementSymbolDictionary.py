2bb6b6f10b60e0db6b52d9c0fb15b5fcf9e26539
ivan.miachikov <ivan.miachikov@gmail.com>
2021-05-14 17:46:34 +0100

    functionality for 2-symbol words, output to console

--- ElementSymbolDictionary/ElementSymbolDictionary.py
+++ ElementSymbolDictionary/ElementSymbolDictionary.py
@@ -1,4 +1,54 @@
 import enchant
 
-d_uk = enchant.Dict("en_UK")
-d_us = enchant.Dict("en_US")
+d = enchant.Dict("en_UK")
+
+
+##check the combination of atomic numbers against the dictionary
+##if found, output the word and atomic number combination
+def checkCombo(combo, elementSymbols, dictionary):
+	
+	symCombo = []
+
+	for i in range(len(combo)):
+		symCombo.append(elementSymbols[combo[i]])
+
+	testWord = "".join(symCombo)
+
+	if dictionary.check(testWord):
+		return print(testWord + ",", combo)
+
+	return 0
+
+
+def addToCombo(combo):
+	
+	for i in range(len(combo)):
+		if i == None:
+			combo[i] = addedSymbol
+	
+	return combo
+
+
+##extract elemental symbols and atomic numbers from txt and put into dictionary
+input_file = open("elemental_symbols.txt", "r")
+
+elementSymbols = {}
+
+for line in input_file:
+	
+	line = line.replace("\n", "")
+	valuePair = line.split(",")
+
+	elementSymbols[int(valuePair[0])] = valuePair[1]
+
+maxSearchLength = 2
+
+##for i in range(1, maxSearchLength):
+for j in range(1, (len(elementSymbols) + 1)):	
+	combo = [0, 0]
+	combo[0] = j
+	
+	for k in range(1, (len(elementSymbols) + 1)):					
+		combo[1] = k
+
+		checkCombo(combo, elementSymbols, d)
